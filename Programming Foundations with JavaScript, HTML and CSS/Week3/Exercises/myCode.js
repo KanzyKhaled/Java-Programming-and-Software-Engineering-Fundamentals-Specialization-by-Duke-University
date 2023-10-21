85d6412d728331cbd65894931d6958081a02171b
Kanzy Khaled <kanzykhaled257@gamil.com>
2023-10-21 22:11:59 +0300

    UPDATE

--- Programming Foundations with JavaScript, HTML and CSS/Week3/Exercises/myCode.js
+++ Programming Foundations with JavaScript, HTML and CSS/Week3/Exercises/myCode.js
@@ -1,85 +1,50 @@
-// write your code here
-var bgimage = new SimpleImage("dinos.png");
-var fgimage = new SimpleImage("drewRobert.png");
-var output = new SimpleImage(fgimage.getWidth(), fgimage.getHeight());
-// missing code
-for(var px of fgimage.values()){
-    if(px.getGreen() > px.getRed() + px.getBlue()){
-        var x = px.getX();
-        var y = px.getY();
-        var bgpixel = bgimage.getPixel(x,y);
-        output.setPixel(x,y,bgpixel);
+
+function makeGrey(Image2){
+  for (var pixel of Image2.values())
+  {
+    var ave = (pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
+    pixel.setRed(ave);
+    pixel.setGreen(ave);
+    pixel.setBlue(ave);
+  }
+  return Image2;
+}
+
+function makeRed(image3){ 
+  for(var px of image3.values()){
+    px.setRed(255);
+}
+  return image3;
+}
+function redHueFilter(Image4){
+  for (var pixel of Image4.values())
+  {
+    var ave = (pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
+    if(ave < 128 ){
+      pixel.setRed(ave*2);
+      pixel.setGreen(0);
+      pixel.setBlue(0);
     }
     else{
-        output.setPixel(px.getX(),px.getY(),px);
+      pixel.setRed(255);
+      pixel.setGreen(ave*2-255);
+      pixel.setBlue(ave*2-255);
     }
-}
-
-print(output);
-//--------------------------------------------------------
-
-var img = new SimpleImage(200,200);
-for (var px of img.values()){
-  var x = px.getX();
-  var y = px.getY();
-  if (x < img.getWidth()/2){
-    px.setRed(255);
   }
-  if (y>img.getHeight()/2){
-    px.setBlue(255);
-  }
-  if(x >= img.getWidth()/2 && y <= img.getHeight()/2 ) {
-    px.setGreen(255);
-  }
-}
-print(img);
-//--------------------------------------------------------
-function setBlack(pixel){
-    pixel.setRed(0);
-    pixel.setGreen(0);
-    pixel.setBlue(0);
-    return pixel;
+  return Image4;
 }
 
-//---------------
 
-function addBorder(image,thickness){
-    height = image.getHeight();
-    width = image.getWidth();
-    for(var px of image.values()){
-        if(px.getY() >= height - thickness || px.getY() <= thickness){
-            setBlack(px);
-        }
-         if(px.getX() >= width - thickness || px.getX() <= thickness){
-            setBlack(px);
-        }
-    }
-    return image;
-}
-
-var image3 = new SimpleImage("astrachan.jpg");
-// missing code
-height = image3.getHeight();
-width = image3.getWidth();
-for(var px of image3.values()){
-    if(px.getY() >= height - 40 || px.getY() <= 40){
-        setBlack(px);
-    }
-     if(px.getX() >= width - 10 || px.getX() <= 10){
-        setBlack(px);
-    }
-}
-print(image3);
-
-//-------------------------------------
-
-var image = new SimpleImage("duke_blue_devil.png");
-for(var px of image.values()){
-    x= px.getX();
-    y= px.getY();
-    if(x>y){
-        px.setRed(0);
-        px.setBlue(0);
-    }
-}
+var image = new SimpleImage("chapel.png");
 print(image);
+print("after grey filter");
+var greyImage = makeGrey(image);
+print(greyImage);
+
+print("after first Red filter");
+var FirstRedImage = makeRed(image);
+print(FirstRedImage);
+
+print("after second Red filter");
+var SecondRedImage = redHueFilter(image);
+print(SecondRedImage);
